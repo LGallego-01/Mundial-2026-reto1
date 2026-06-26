07fdd5e97a2779d6e425cc680830f646504f55c3
LGallego-01 <lilianagallegoq@gmail.com>
2026-06-26 15:26:18 -0500

    colocar grupo de partido

--- assets/js/app.js
+++ assets/js/app.js
@@ -45,6 +45,9 @@ function matchCard(match) {
 
   return `
     <article class="card">
+      <small class="match-group">
+         ${formatStage(match.stage)}
+      <small>
       <span class="pill">${statusLabel(statusOf(match))}</span>
       <h3>${teamName(match.homeTeam)} vs ${teamName(match.awayTeam)}</h3>
       <div class="score">${homeScore} - ${awayScore}</div>
